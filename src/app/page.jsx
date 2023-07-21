3e4b227c87100fa5f1e771ef8a5a1393c61f76ce
Garcia-Sebastiao <garciasebastiao.php@gmail.com>
2023-07-21 15:11:04 +0100

    Esclarecimento de acesso.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,13 @@
 import Link from "next/link";
+import { useCallback } from "react";
 
 export default function Home() {
   return (
-    <main>
-      <Link href="/system/">Sistema.</Link>
+    <main className="flex items-center justify-center h-screen">
+      <Link className="text-black" href="/system/">
+        O Crisvan ainda não fez uma tela de login. Clique aqui para ter acesso
+        ao sistema.
+      </Link>
     </main>
   );
 }
